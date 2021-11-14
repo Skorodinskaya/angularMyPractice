20ce190754ca1e82aff4769e06d0a64cfd7feec7
Skorodinskaya <skorodinskaya01@gmail.com>
2021-11-14 15:13:32 +0200

    my practice of homework 1 and classwork 1

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from "../../modules/post";
+import {PostService} from "../../services/post.service";
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  posts: Post[] = [];
+  @Input()
+  id: number;
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.postService.getPost(this.id).subscribe(value => {this.posts = value});
+  }
+
+}
